8813d4034938a50e25c547da3c4f18be9d76fe17
Ben Pence <email@benpence.com>
2011-07-17 09:52:17 -0700

    Removed debug attribute.

--- app/dontspamme/config.py
+++ app/dontspamme/config.py
@@ -1,5 +1,3 @@
-import os
-
 # Where non-users are referred to if they're logged in
 referral_for_non_users = "www.google.com" 
 
@@ -8,8 +6,8 @@ spam_label = '(SPAM)'
 
 
 # CHANGE THESE IF YOU KNOW WHAT YOU'RE DOING
-app = os.getcwd().split('/')[-2]
+import os
 
-DEBUG = True
+app = os.getcwd().split('/')[-2]
 web_domain = app + '.appspot.com'
 mail_domain = app + '.appspotmail.com'
